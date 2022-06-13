27792025c639d1b3335b52194f55a68b1d61659e
hernan2626 <hernangasanol22@outlook.com>
2022-06-13 17:42:22 -0300

    css error fixed

--- src/components/main/MainContainer.jsx
+++ src/components/main/MainContainer.jsx
@@ -1,34 +1,18 @@
 import React from 'react'
-// import {getMovies} from '../../services/services'
 import {useState ,useEffect } from  'react'
 import Main from './Main'
 import { moviesSliderData } from '../../../data'
-import '../slider/Carrousel.css'
+// import '../slider/Carrousel.css'
 
 import { categoriesData, headerData } from '../../../data'
 
 const MainContainer = () => {
   
-  // const [movies, setMovies] = useState([]);
-
-
-  // useEffect(() => {
-
-  //   getMovies("discover/movie").then((res) => setMovies(res.results));
-  // }, []);
-
-
-  
-
 
  return (
    <main className="w-full absolute z-20 top-[50vw]  text-white text-xl font-sans font-medium ">
      <div className="flex flex-col  pl-8">
-       {/* <div>
-         <Main category="Tendencias" data={movies} />
-       </div> */}
-
-       <div>
+           <div>
          {categoriesData.map(item => (
              <Main category={item.title} data={moviesSliderData} />
          ))}
